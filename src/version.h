44e4bd0b5454ff8aee1ff3376974dfe6014300d9
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2024-08-19 07:55:57 +0200

    CI updates

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x040300 /* 04.03.00 */
 #define UPX_VERSION_STRING   "4.3.0"
 #define UPX_VERSION_STRING4  "4.30"
-#define UPX_VERSION_DATE     "Jun 19th 2024"
-#define UPX_VERSION_DATE_ISO "2024-06-19"
+#define UPX_VERSION_DATE     "Aug 19th 2024"
+#define UPX_VERSION_DATE_ISO "2024-08-19"
 #define UPX_VERSION_YEAR     "2024"
