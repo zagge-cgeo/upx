3798dde3aad34f2bcd98e1f578748f94550a009c
John Reiser <jreiser@BitWagon.com>
2024-11-19 08:44:48 -0800

    Fix stubs in arm,mips,powerpc for upx_mmap_and_fd
    	modified:   stub/Makefile
    	modified:   stub/src/arm.v4a-expand.S
    	modified:   stub/src/arm.v4a-linux.elf-entry.S
    	modified:   stub/src/mips.r3000-expand.S
    	modified:   stub/src/mipsel.r3000-linux.elf-entry.S
    	modified:   stub/src/powerpc-linux.elf-fold.S
    	modified:   stub/src/upxfd_linux.c

--- src/stub/src/upxfd_linux.c
+++ src/stub/src/upxfd_linux.c
@@ -6,6 +6,76 @@
    All Rights Reserved.
  */
 
+#if defined(__i386__) //}{
+#define ANDROID_FRIEND 1
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("call 0f; .asciz \"" string "\"; 0: pop %0" \
+/*out*/ : "=r"(str) ); \
+    str; \
+})
+#elif defined(__arm__) //}{
+#define ANDROID_FRIEND 1
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("bl 0f; .string \"" string "\"; .balign 4; 0: mov %0,lr" \
+/*out*/ : "=r"(str) \
+/* in*/ : \
+/*und*/ : "lr"); \
+    str; \
+})
+#elif defined(__mips__) //}{
+#define ANDROID_FRIEND 0
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("bal 0f; .asciz \"" string "\"; .balign 4\n0: move %0,$31" \
+/*out*/ : "=r"(str) \
+/* in*/ : \
+/*und*/ : "ra"); \
+    str; \
+})
+#elif defined(__powerpc__)  /*}{*/
+#define ANDROID_FRIEND 0
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("bl 0f; .asciz \"" string "\"; .balign 4; 0: mflr %0" \
+/*out*/ : "=r"(str) \
+/* in*/ : \
+/*und*/ : "lr"); \
+    str; \
+})
+#elif defined(__powerpc64__) //}{
+#define ANDROID_FRIEND 0
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("bl 0f; .string \"" string "\"; .balign 4; 0: mflr %0" \
+/*out*/ : "=r"(str) \
+/* in*/ : \
+/*und*/ : "lr"); \
+    str; \
+})
+#elif defined(__x86_64) //}{
+#define ANDROID_FRIEND 0
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("lea 9f(%%rip),%0; .section STRCON; 9:.asciz \"" string "\"; .previous" \
+/*out*/ : "=r"(str) ); \
+    str; \
+})
+#elif defined(__aarch64__) //}{
+#define ANDROID_FRIEND 0
+#define addr_string(string) ({ \
+    char const *str; \
+    asm("bl 0f; .string \"" string "\"; .balign 4; 0: mov %0,x30" \
+/*out*/ : "=r"(str) \
+/* in*/ : \
+/*und*/ : "x30"); \
+    str; \
+})
+#else  //}{
+#define ANDROID_FRIEND 0
+#error  addr_string
+#endif  //}
 #include "include/linux.h"  // syscall decls; i386 inlines via "int 0x80"
 #define MFD_EXEC 0x10
 //#define O_RDWR 2
@@ -26,14 +96,13 @@ unsigned long upx_mmap_and_fd_linux( // returns (mapped_addr | (1+ fd))
 )
 {
     (void)pathname;  // FIXME NYI
-    char str_upx[] = {'u','p','x',0};
-    int fd = memfd_create(str_upx, MFD_EXEC);
+    char const *const name = addr_string("upx");
+    int fd = memfd_create(name, MFD_EXEC);
     if (-EINVAL == fd) { // 2024-10-15 MFD_EXEC unknown to ubuntu-20.04
-        fd = memfd_create(str_upx, 0);  // try again
+        fd = memfd_create(name, 0);  // try again
     }
     if (fd < 0) { // last chance for Linux
-        char str_dev_shm[] = {'/','d','e','v','/','s','h','m', 0};
-        fd = open(str_dev_shm, O_RDWR | O_DIRECTORY | O_TMPFILE, 0700);
+        fd = open(addr_string("/dev/shm"), O_RDWR | O_DIRECTORY | O_TMPFILE, 0700);
         if (fd < 0) {
             return (unsigned long)(long)fd;
         }
